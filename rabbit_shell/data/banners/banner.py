ede2a39d0b33d64f6fcaead74a41758f00d7637d
= <albadiwolf@gmail.com>
2022-05-30 14:25:35 -0700

    added default values to main_banner fun

--- rabbit_shell/data/banners/banner.py
+++ rabbit_shell/data/banners/banner.py
@@ -5,9 +5,10 @@ import sys
 import colorama
 from rabbit_shell import fixed_path, clear
 import rabbit_shell
+from rabbit_shell.data import basic, about
 import os
 colorama.init()
-def main_banner(host, port, version, name):
+def main_banner(host=basic.HOST, port = basic.PORT, version=about.__name__, name=about.__version__):
     file = open(fixed_path(f"{os.path.dirname(rabbit_shell.__file__)}\\data\\banners\\rabbit.txt"), "r")
     banner = colorama.Fore.LIGHTGREEN_EX + str(file.read())
     banner = banner.replace("NAME", str(name))
